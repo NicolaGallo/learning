f6483b92be40b754bb5233c6cd4e9223393627ec
NicolaGallo <nicola.gallo89@gmail.com>
2017-06-21 13:25:56 +0200

    angular lesson 2.1 and 2.2

---
+++ ANGULAR_JS/01_04/start/angular/js/controllers.js
@@ -0,0 +1,15 @@
+var myApp= angular.module('myApp', []);
+
+myApp.controller('MyController', function MyController($scope){
+
+$scope.author = {
+
+    'name': 'Nicola',
+    'title':'Software developer',
+    'company' : 'Ericsson'
+}
+
+
+
+
+});
